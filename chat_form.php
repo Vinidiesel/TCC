0ec68ec525dcf99eda9fe5d272e1351875c48063
Nicolas008 <91225465+Nicolas008@users.noreply.github.com>
2021-11-03 14:34:10 -0300

    wdcxfedcfec
    ed fev fe

--- chat_form.php
+++ chat_form.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="estilos/estilo.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <?php include_once "topo.php"; ?>
@@ -41,9 +42,9 @@
         }
         ?>
         <br><br/>
-        <form action="" method="post">
+        <form action="" method="post">>
         <td><textarea placeholder="Conversar" class="chat" name="texto" rows="1" cols="30"></textarea>
-        <input class="chat" type="submit" value="Enviar">
+        <input class="chat" type="submit" value="Enviar" style="position: fixed; top: 20">
         <p>
         <?php echo voltar();?>
 
