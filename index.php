ec449b353fe50a79cf43f1c9d2e40850796290d9
Vinicius Inhesta Dos Santos <91141292+Vinidiesel@users.noreply.github.com>
2021-11-10 20:41:31 -0300

    navbar
    olaaaaaaa

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="estilos/estilo.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
 
 img.mini{
@@ -17,6 +18,7 @@ img.full{
 </style>
 </head>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <?php
     /*Com incluide se ele não encontrar o banco ele continua o site
     já com o require se ele não acha o site mata o resto*/
--- topo.php
+++ topo.php
@@ -1,8 +1,66 @@
-<?php
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <?php
+        if($_SESSION['email']){
+    $busca = $banco->query("SELECT * from usuario WHERE NOME='".$_SESSION['nome']."'");
+    $reg=$busca->fetch_object();
+    echo "<li><a href='chat.php' class='nav-link px-2 text-white'> Chat </a></li>";
+    if(is_admin()){
+    echo "<li><a href='user_new_admin.php' class='nav-link px-2 text-white'> Novo usuario </a></li>";
+    echo "<li><a href='noticia_new.php' class='nav-link px-2 text-white'> Nova Noticia </a></li>";
+}
+}
+?>
+          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+        </ul>
+
+        <div class="text-end">
+        <?php
 echo "<header>";
 
 if(empty($_SESSION['email'])){
-echo "<a href='user_login.php'>Entrar</a>";
+echo "<a href='user_login.php'><button type='button' class='btn btn-outline-light me-2'>Entrar</button></a>";
+echo "<a href='user_new.php'><button type='button' class='btn btn-warning'>Sign-up</button></a>";
+}else{
+    echo "<a href='dados.php?cod=$reg->ID_USUARIO'><button type='button' class='btn btn-outline-light me-2'>Olá ". $_SESSION['nome']."</button></a>";
+    echo "<a href='user_logout.php'><button type='button' class='btn btn-warning'>Sair</button></a>";
+}
+?>
+<?php /*else{
+    $busca = $banco->query("SELECT * from usuario WHERE NOME='".$_SESSION['nome']."'");
+    $reg=$busca->fetch_object();
+echo "Olá ". $_SESSION['nome']."</strong> |";
+ECHO "<a href='dados.php?cod=$reg->ID_USUARIO'><button type='button' class='btn btn-outline-light me-2'> Meus Dados </button></a>|";
+echo "<a href='chat.php'> Chat </a>|";
+if(is_admin()){
+    echo "<a href='user_new_admin.php'> Novo usuario </a>| ";
+    echo "<a href='noticia_new.php'> Nova Noticia </a> |";
+}
+echo "<a href='user_logout.php'>Sair</a>";
+}
+
+echo "</header>";
+*/?>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
+
+<?php 
+/*
+echo "<header>";
+
+if(empty($_SESSION['email'])){
+echo "<a href='user_login.php'><button type='button' class='btn btn-outline-light me-2'>Entrar</button></a>";
 }
 
 else{
@@ -19,4 +77,4 @@ echo "<a href='user_logout.php'>Sair</a>";
 }
 
 echo "</header>";
-?>
+*/?>
